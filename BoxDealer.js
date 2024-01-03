d98056794bec6f5057dffcc93668bc43db7e799d
xingluonapan2010 <3101545791@qq.com>
2024-01-03 22:49:45 +0800

    v1.0.1 2024.1.3 解决点击title也会弹出提示正在developing的问题

--- BoxDealer.js
+++ BoxDealer.js
@@ -1,8 +1,9 @@
 // JavaScript source code
 var AllBox = document.getElementsByClassName('box')
+var NoRemindBox = document.getElementById('NoRemind')
 
 for (var i = 0; i < AllBox.length; i++) {
-    if (AllBox[i]!=document.getElementById('NoRemind')) {
+    if (AllBox[i] != NoRemindBox) {
         AllBox[i].addEventListener('click', function () {
             alert('This page is still developing.')
         })
